32a2deb101badcc32fb1b3f4bb822c07a090448e
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2021-09-13 13:10:19 +0600

    Update message.php

--- message.php
+++ message.php
@@ -21,11 +21,5 @@ if(mysqli_num_rows($run_query) > 0){
     echo "Sorry can't be able to understand you!";
 }
 
-//kisu akta
-<<<<<<< Updated upstream
-//adaddashdashjkd
-//date_isodate_set
-=======
-//new b
->>>>>>> Stashed changes
+
 ?>
